76fde3be546f339f217b0b47ebca36df698e513b
1111 <a1111@MacBook-Pro-1111.local>
2025-07-14 13:48:57 +0300

    first commit

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -1,15 +1,19 @@
 import * as fs from "fs";
 import * as path from "path";
-import { Address, contractAddress } from "@ton/core";
-import { SampleTactContract } from "./output/sample_SampleTactContract";
+import { Address, contractAddress, toNano } from "@ton/core";
+import { Lottery } from "./output/sample_Lottery";
 import { prepareTactDeployment } from "@tact-lang/deployer";
 
 (async (): Promise<void> => {
     // Parameters
     let testnet = true;
-    let packageName = "sample_SampleTactContract.pkg";
-    let owner = Address.parse("kQBM7QssP28PhrctDOyd47_zpFfDiQvv5V9iXizNopb1d2LB");
-    let init = await SampleTactContract.init(owner, 0n);
+    let packageName = "sample_Lottery.pkg";
+    let owner = Address.parse("0QClacqt1G1VQ9sasHogYbaMn0pZJdTKnrZBfYmDXOELmu4E");
+    let init = await Lottery.init(
+    toNano("1"),        // bid = 1 TON
+    toNano("0.5"),      // minPrize = 0.5 TON
+    toNano("1.01")      // maxPrize = 1.01 TON 
+    );
 
     // Load required data
     let address = contractAddress(0, init);
